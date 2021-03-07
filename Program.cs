0700f93c1885c44769e59f9ccd2b5dcf7afa3ce3
cannow <49660002+cannow@users.noreply.github.com>
2021-03-07 16:22:40 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StringOrdering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pangram = "The quick and also brown fox jumped over the lazy bones doggie dog";
+            string[] words = pangram.Split();
+
+            foreach(var word in words)
+            {
+                char[] letters = word.ToCharArray();
+                Array.Reverse(letters);
+                Console.Write(letters);
+                Console.Write(" ");
+            }
+
+            Console.Write("\n");
+
+            string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+            string[] orders = orderStream.Split(',');
+
+            foreach(var order in orders)
+            {
+                if(order.Contains("B"))
+                {
+                    Console.WriteLine(order);
+                }
+            }
+        }
+    }
+}
