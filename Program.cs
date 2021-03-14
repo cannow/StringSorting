5cf07962bea04dc8f8c5a4ccad2228048bb45f5b
cannow <49660002+cannow@users.noreply.github.com>
2021-03-14 13:45:27 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace StringOrdering
 {
@@ -6,33 +7,65 @@ namespace StringOrdering
     {
         static void Main(string[] args)
         {
-            string pangram = "The quick and also brown fox jumped over the lazy bones doggie dog";
-            string[] words = pangram.Split();
+            Console.WriteLine("Alright so first let's have you type in a sentence, then I'll reverse the letters but not the word order...\n");
+            string pangram = Console.ReadLine();
+            LetterReversal(pangram);
+            
+            Console.WriteLine("\n\nAin't that neat?\nOk this here's a two parter, first type in a list of things seperated by a comma...\n");
+            string orderStream = Console.ReadLine();
+            string[] orders = orderStream.Split(',');
+            OrderFinder(orders);
+
+            Console.WriteLine("Fun stuff huh? Okay this is the last one, and it's another two parter.\nFirst let's have you write two expressions, then I'll:\n\t1) Add them together\n\t2) Sort it alphabetically\n\t3) Get rid of any duplicate characters you've typed!\nFirst expression...");
+            string alphA = Console.ReadLine();
+            Console.WriteLine("Now second...");
+            string alphB = Console.ReadLine();
+            Alphabatizer(alphA, alphB);
+
+            Console.WriteLine("Okay it's over, press enter to exit");
+            Console.ReadLine();
+        }
+
+        public static string LetterReversal(string sentence)
+        {
+            string[] words = sentence.Split();
+            string output = "";
 
             foreach(var word in words)
             {
                 char[] letters = word.ToCharArray();
                 Array.Reverse(letters);
+                output += letters;
+
                 Console.Write(letters);
                 Console.Write(" ");
             }
 
-            Console.Write("\n");
+            return output;
+        }
 
-            string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-            string[] orders = orderStream.Split(',');
+        public static string OrderFinder(string[] orders)
+        {
+            string output = "";
+
+            Console.WriteLine("\nAwesome now type in what you'd like to search for in that list you just typed:\n");
+            string searchItem = Console.ReadLine();
+            byte foundCount = 0;
 
             foreach(var order in orders)
             {
-                if(order.Contains("B"))
+                if(order.Contains(searchItem))
                 {
-                    Console.WriteLine(order);
+                    output += $"Found : {order}\n";
+                    foundCount++;
                 }
             }
-
-            Longest("aabbccddeeffgghhiijjkkllmmnnoopp", "qqrrssttuuvvwwxxyyzzakfs");
+            
+            Console.WriteLine(foundCount == 0  || searchItem == "" ? "\nWow we found nothing!\n" : $"\n{output.Trim(' ')}\n");
+            return output;
         }
-        public static string Longest(string s1, string s2)
+
+        public static string Alphabatizer(string s1, string s2)
         {
             char[] letters = (string.Concat(s2, s1)).ToCharArray();
             Array.Sort(letters);
@@ -46,8 +79,9 @@ namespace StringOrdering
                 }
             }
 
-            Console.WriteLine(output);
+            Console.WriteLine($"\n{output.Trim(' ')}");
             return output;
         }
+
     }
 }
